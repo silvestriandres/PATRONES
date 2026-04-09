42ba7704e2f809f3a1d193a84149fe5475c0a3af
silvestriandres <silvestri.andres@gmail.com>
2026-04-09 19:29:01 -0300

    test: add unit tests for payment strategy selector

---
+++ src/Behavioral/Strategy/StrategyPattern.Tests/PaymentStrategySelectorTests.cs
@@ -0,0 +1,33 @@
+﻿using StrategyPattern.ConsoleProject.Domain.Enums;
+using StrategyPattern.ConsoleProject.Helpers;
+using StrategyPattern.ConsoleProject.Strategies;
+using Xunit;
+
+namespace StrategyPattern.Tests;
+
+public class PaymentStrategySelectorTests
+{
+    [Fact]
+    public void Should_Return_CreditCardStrategy()
+    {
+        var strategy = PaymentStrategySelector.GetStrategy(PaymentMethod.CreditCard);
+
+        Assert.IsType<CreditCardPaymentStrategy>(strategy);
+    }
+
+    [Fact]
+    public void Should_Return_PayPalStrategy()
+    {
+        var strategy = PaymentStrategySelector.GetStrategy(PaymentMethod.PayPal);
+
+        Assert.IsType<PayPalPaymentStrategy>(strategy);
+    }
+
+    [Fact]
+    public void Should_Return_CryptoStrategy()
+    {
+        var strategy = PaymentStrategySelector.GetStrategy(PaymentMethod.Crypto);
+
+        Assert.IsType<CryptoPaymentStrategy>(strategy);
+    }
+}
