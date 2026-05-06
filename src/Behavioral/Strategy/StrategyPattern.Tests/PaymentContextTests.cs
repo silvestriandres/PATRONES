6ba030081b4df2224497fdf4c13f6c5172ba8108
asilvestri <asilvestri@seguridadvial.gob.ar>
2026-05-06 18:59:09 -0300

    test: add unit tests for Strategy pattern selector and context

---
+++ src/Behavioral/Strategy/StrategyPattern.Tests/PaymentContextTests.cs
@@ -0,0 +1,29 @@
+﻿using StrategyPattern.ConsoleProject.Context;
+using StrategyPattern.ConsoleProject.Domain.Interfaces;
+using Xunit;
+
+namespace StrategyPattern.Tests;
+
+public class PaymentContextTests
+{
+    private class FakeStrategy : IPaymentStrategy
+    {
+        public bool WasCalled { get; private set; }
+
+        public void ProcessPayment(decimal amount)
+        {
+            WasCalled = true;
+        }
+    }
+
+    [Fact]
+    public void Should_Execute_Strategy()
+    {
+        var fake = new FakeStrategy();
+        var context = new PaymentContext(fake);
+
+        context.ExecutePayment(100);
+
+        Assert.True(fake.WasCalled);
+    }
+}
